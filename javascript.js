0584d43ba128cd63e6e7ea04d9a80a834ce1f1ed
matt-coops <matt.cooper.85@gmail.com>
2023-05-15 13:02:20 +1000

    First commit

---
+++ javascript.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const gameboard = (() => {
+    const top-left = "";
+    const top-right = "";
+  return {};
+})();
+
+function createPlayer(name, marker) {
+  return { name, marker };
+}
+
+const player1 = createPlayer("james", "X");
+
+console.log(player1);
